d819625e84cb985f8d55465c719401de6c5e601c
Tarun Nayaka R <97576326+Rtarun3606k@users.noreply.github.com>
2024-11-08 19:33:50 +0530

    Comment out the Flask app's debug mode in main execution block

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 def home():
     return jsonify({'message': 'Hello World!'})
 
-if __name__ == '__main__':
-    app.run(debug=True , port=8080, host='0.0.0.0')
+# if __name__ == '__main__':
+#     app.run(debug=False , port=8080, host='0.0.0.0')
