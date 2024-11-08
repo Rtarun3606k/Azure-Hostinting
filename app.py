d4c0b078dbb0f9bd98068dbb1360558f6b882547
Tarun Nayaka R <97576326+Rtarun3606k@users.noreply.github.com>
2024-11-08 19:21:39 +0530

    Update home route to return JSON response instead of rendering HTML

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask , render_template
+from flask import Flask , render_template,jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return jsonify({'message': 'Hello World!'})
 
 if __name__ == '__main__':
     app.run(debug=True , port=8080, host='0.0.0.0')
